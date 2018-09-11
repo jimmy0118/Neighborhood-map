def101dea052c61ce517ace1678eeff50711220c
Jimmy Weng <opep0000@gmail.com>
2018-09-11 16:34:17 +0800

    Adding markers to js file

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
 // Global Varialbes
 var map;
 
+var markers = [];
+// Google maps init
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 25.061136, lng: 121.525712},
-    zoom:13,
+    zoom:10,
     mapTypeControl: false
   });
+
+  var bounds = new google.maps.LatLngBounds();
+
+  for (var i = 0; i < locations.length; i++) {
+          // Get the position from the location array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          // Create a marker per location, and put into markers array.
+          var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open an infowindow at each marker.
+          bounds.extend(markers[i].position);
+        }
+        // Extend the boundaries of the map for each marker
+        map.fitBounds(bounds);
+}
+
+// Handle map error
+function googleError() {
+    window.alert('An error occurred with Google Maps!')
 }
--- js/data.js
+++ js/data.js
@@ -7,5 +7,54 @@ var locations = [
             lat: 24.997158,
             lng: 121.585151
         }
+    },
+    {
+        title: 'Taipei 101',
+        location: {
+            lat: 25.033964,
+            lng: 121.564472
+        }
+    },
+    {
+        title: 'Chiang Kai-shek Memorial Hall',
+        location: {
+            lat: 25.034612,
+            lng: 121.521781
+        }
+    },
+    {
+        title: 'The Grand Hotel',
+        location: {
+            lat: 25.078304,
+            lng: 121.526137
+        }
+    },
+    {
+        title: '85 Sky Tower',
+        location: {
+            lat: 22.611598,
+            lng: 120.300196
+        }
+    },
+    {
+        title: 'Yehliu Geopark',
+        location: {
+            lat: 25.208805,
+            lng: 121.693102
+        }
+    },
+    {
+        title: 'Eluanbi Lighthouse',
+        location: {
+            lat: 21.902242,
+            lng: 120.852626
+        }
+    },
+    {
+        title: 'Sun Moon Lake',
+        location: {
+            lat: 23.857334,
+            lng: 120.915913
+        }
     }
 ];
