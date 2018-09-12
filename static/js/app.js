8d7ab2141008dc46169b391a44dccbf6c97bc655
Jimmy Weng <opep0000@gmail.com>
2018-09-12 15:32:15 +0800

    Implement flask framework and adding foursquare api

--- static/js/app.js
+++ static/js/app.js
@@ -14,18 +14,36 @@ function initMap() {
   var largeInfowindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
 
+
   // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
           // Get the position from the location array.
           var position = locations[i].location;
           var title = locations[i].title;
+          var address = '';
+          var city = '';
+          var country = '';
+
+          var clientID = 'S4QE4Z25BER33C3JVBNJIFYZS4YHZ0Y4X1SAENVIQHNNAIV1';
+          var clientSecret = 'WJNLUZTKA135VHB15B2IKPYXCOOT2DXQSWDLHFIKMKKSEZRA';
+
+          // Get JSON request of foursquare data
+          var fsurl = 'https://api.foursquare.com/v2/venues/search?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20180323&limit=1&ll=' + position.lat + ',' + position.lng +'&query=' + title;
+
+          $.getJSON(fsurl).done(function(data) {
+              var results = data.response.venues[0];
+              address = results.location.formattedAddress[0] ? results.location.formattedAddress[0]: 'N/A';
+              city = results.location.formattedAddress[1] ? results.location.formattedAddress[1]: 'N/A';
+              country = results.location.formattedAddress[2] ? results.location.formattedAddress[2]: 'N/A';
+          }).fail(function() {
+              window.alert('Something went wrong with foursquare');
+          });
+
           // Create a marker per location, and put into markers array.
           var marker = new google.maps.Marker({
-            map: map,
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
-            id: i
           });
           // Push the marker to our array of markers.
           markers.push(marker);
@@ -33,7 +51,7 @@ function initMap() {
           bounds.extend(markers[i].position);
           // Create an onclick event to open the large infowindow at each marker.
           marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
+            populateInfoWindow(this, address, city, country, largeInfowindow);
           });
         }
         // Extend the boundaries of the map for each marker
@@ -46,7 +64,7 @@ function googleError() {
 }
 
 // This function populates the infowindow when the marker is clicked.
-function populateInfoWindow(marker, infowindow) {
+function populateInfoWindow(marker, address, city, country, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
       // Clear the infowindow content to give the streetviwew time to load.
@@ -56,8 +74,14 @@ function populateInfoWindow(marker, infowindow) {
       infowindow.addListener('closeclick', function() {
           infowindow.marker = null;
       });
+      // Set the content of infowindow
+      windowContent = '<h3>' + marker.title + '</h3>' + '<p>' + address + '</br>' + city + '</br>' + country + '</p>';
+
       var streetViewService = new google.maps.StreetViewService();
-      var radius = 200;
+      var radius = 150;
+
+      // Set the content of
+
       // In case the status is OK, which means the pano was found, compute the
       // position of the streetview image, then calculate the heading, then get a
       // panorama from that and set the options
@@ -66,7 +90,7 @@ function populateInfoWindow(marker, infowindow) {
               var nearStreetViewLocation = data.location.latLng;
               var heading = google.maps.geometry.spherical.computeHeading(
                   nearStreetViewLocation, marker.position);
-                  infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+                  infowindow.setContent(windowContent + '</div><div id="pano"></div>');
                   var panoramaOptions = {
                     position: nearStreetViewLocation,
                     pov: {
@@ -77,11 +101,11 @@ function populateInfoWindow(marker, infowindow) {
               var panorama = new google.maps.StreetViewPanorama(
                   document.getElementById('pano'), panoramaOptions);
           } else {
-              infowindow.setContent('<div>' + marker.title + '</div>' + '<div>No Street View Found</div>');
+              infowindow.setContent(windowContent + '<div>No Street View Found</div>');
           }
       }
       // Use streetview service to get the closest streetview image within
-      // 50 meters of the markers position.
+      // 150 meters of the markers position.
       streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
       // open the infowindow on the correct marker.
       infowindow.open(map, marker);
