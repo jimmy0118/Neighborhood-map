10c2adbca44e468970a15c5fbf1b78e64e660157
Jimmy Weng <opep0000@gmail.com>
2018-09-13 16:33:54 +0800

    Editing css

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,16 @@
+// Open the drawer when the menu ison is clicked.
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+});
+
 // Global Varialbes
 var map;
 var largeInfowindow;
